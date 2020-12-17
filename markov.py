87e90c3e5b9587abb0d708249085a9a134ed77d9
torikatsu <torikatsu923@gmail.com>
2020-12-17 17:31:55 +0900

    render directed graph

--- markov.py
+++ markov.py
@@ -1,15 +1,18 @@
 import pandas as pd
 from collections import defaultdict
-from pandas.core.frame import DataFrame
+import seaborn as sns
+import matplotlib.pyplot as plt
+from graphviz import Digraph
 
 index = [str(i) for i in range(1, 31)]
 
-def read_csv(csv_path: str, range_number: int) -> DataFrame:
+def read_csv(csv_path: str, range_number: int) -> pd.DataFrame:
   return pd.read_csv(csv_path, usecols=range(range_number), low_memory=False, dtype="object")
 
-def main(df: DataFrame, file_name: str) -> None:
-  df = df[index]
+def main(df: pd.DataFrame, file_name: str) -> None:
+  df = df[index].dropna(subset=[index[0]])
   count = defaultdict(lambda:defaultdict(lambda:0))
+
   # 品詞の前後関係をカウント
   for loc in df.index.values:
     row = df.loc[loc]
@@ -23,15 +26,33 @@ def main(df: DataFrame, file_name: str) -> None:
 
   # 確率を算出
   result = pd.DataFrame()
-  count_df = pd.DataFrame(count).fillna(0)
+  count_df = pd.DataFrame(count).fillna(0).transpose()
   for loc in count_df.index.values:
     row = count_df.loc[loc]
     sum = row.sum()
     new_row = row.apply(lambda x: round(x / sum, 4) if x != 0 else 0)
     result = pd.concat([result, new_row], axis=1)
-
   result.to_csv(file_name)
 
-main(read_csv("../csv/modified/class_morphological.csv", 72), "./class_markov.csv")
-main(read_csv("../csv/modified/method_morphological.csv", 73), "./method_markov.csv")
-main(read_csv("../csv/modified/variable_morphological.csv", 74), "./variable_markov.csv")
+def render_heatmap(csv_path: str, file_name: str):
+  df = pd.read_csv(csv_path, index_col=0)
+  df.fillna("NaN")
+  plt.figure()
+  sns.heatmap(df, xticklabels=1, yticklabels=1)
+  plt.savefig(file_name)
+  plt.close("all")
+
+def render_tree(csv_path: str, file_name: str):
+  dg = Digraph(comment='The Round Table')
+  df = pd.read_csv(csv_path, index_col=0)
+  for column in df.columns:
+    for index in df.index:
+      value = df.at[index, column]
+      if value >= 0.05:
+         dg.edge(column, index, label=str(round(value, 3)))
+  dg.render(file_name + '.gv', view=True)
+
+# test
+render_tree("../csv/markov/class_markov.csv",     "class_markov_graph")
+render_tree("../csv/markov/method_markov.csv",    "method_markov_graph")
+render_tree("../csv/markov/variable_markov.csv",  "variable_markov_graph")
